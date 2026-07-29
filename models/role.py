cda515781aa9f02a8f1ade20044064fef12aeea8
Dominik Farlík <dominik@farlik.cz>
2026-07-29 22:06:52 +0200

    add enum and response model for role table

--- models/role.py
+++ models/role.py
@@ -1,9 +1,17 @@
+from enum import StrEnum
 from typing import List, TYPE_CHECKING
 
+from pydantic import BaseModel
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from models import Base
+
+
+class RoleEnum(StrEnum):
+    VIEWER = "viewer"
+    EDITOR = "editor"
+    MANAGER = "manager"
 
 if TYPE_CHECKING:
     from models.user import User
@@ -13,6 +21,13 @@ class Role(Base):
     __tablename__ = "role"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(50), unique=True, index=True)
+    name: Mapped[RoleEnum] = mapped_column(String(50), unique=True, index=True)
 
     users: Mapped[List["User"]] = relationship(back_populates="role")
+
+
+class RoleResponse(BaseModel):
+    name: str
+
+    class Config:
+        from_attributes = True
